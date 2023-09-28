e709b7783e76fb26d30bc63135ee245a8ee83b3a
FrenzyCT <111730227+FrenzyCT@users.noreply.github.com>
2023-09-28 15:45:20 +0200

    changed wrong text in print statement

--- leapyear/src/leap_year.py
+++ leapyear/src/leap_year.py
@@ -9,8 +9,8 @@ def calculate_if_leap_year(leapyear):
 
 
 if __name__ == "__main__":
-    print("hva year do you whant to check for leapyear?: ")
+    print("Type the year you want to check if it's a leap year: ")
     year = int(input())
 
     isLeapyear = calculate_if_leap_year(year)
-    print(f"Is {year} a leapyear?: {isLeapyear}")
+    print(f"Is {year} a leap year?: {isLeapyear}")
