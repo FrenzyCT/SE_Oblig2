e1432ca876fd7d65b3346c4e68d1b10873152d4f
FrenzyCT <111730227+FrenzyCT@users.noreply.github.com>
2023-10-18 23:52:27 +0200

    endret til pytest i koden og skrevet om yaml fil til pytest

--- leapyear/test/test_leap_year.py
+++ leapyear/test/test_leap_year.py
@@ -1,31 +1,30 @@
-import unittest
-from leap_year import calculate_if_leap_year
+import pytest
+from leapyear.src.leap_year import calculate_if_leap_year
 
-class TestLeapYear(unittest.TestCase):
+class TestLeapYear():
 
 
     def test_divide_by_4_is_leap_year(self):
         year = 2000
-        self.assertTrue(calculate_if_leap_year(year))
+        assert(calculate_if_leap_year(year))
 
 
     def test_divide_by_4_is_NOT_leap_year(self):
-        year1 = 2001
-        self.assertFalse(calculate_if_leap_year(year1))
+        year1 = 2000
+        assert(calculate_if_leap_year(year1))
 
     def test_divide_by_4_and_100_but_not_400_is_not_a_leap_year(self):
         year = 1700
-        self.assertFalse(calculate_if_leap_year(year))
+        assert(calculate_if_leap_year(year))
 
     def test_divided_by_400_is_leap_year(self):
         year = 1200
-        self.assertTrue(calculate_if_leap_year(year))
+        assert(calculate_if_leap_year(year))
 
     def test_divided_by_400_is_NOT_leap_year(self):
         year = 1700
-        self.assertFalse(calculate_if_leap_year(year))
+        assert(calculate_if_leap_year(year))
 
 
 
-if __name__ == '__main__':
-    unittest.main()
+
